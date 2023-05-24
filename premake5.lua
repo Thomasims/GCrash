03c3748467622b2b9f4c93a90cc122ffbdb548d4
Thomasims <thomasims3@hotmail.fr>
2023-05-24 21:44:17 +0200

    x86-64 support [lua_shared.so not included]

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,15 @@
 workspace "gmsv_gcrash"
-	configurations { "Debug", "Release" }
+	configurations { "Debug", "Debug64", "Release", "Release64" }
 	location ( "projects/" .. os.target() )
 
 project "gmsv_gcrash"
 	kind         "SharedLib"
-	architecture "x86"
 	language     "C++"
 	includedirs  "include/"
 	libdirs      "libs/"
 	targetdir    "build"
 
-	if os.istarget( "windows" ) then targetsuffix "_win32" end
-	if os.istarget( "macosx" )  then targetsuffix "_osx"   end
-	if os.istarget( "linux" )   then targetsuffix "_linux" end
-
 	if _ACTION == "gmake2" then
-		-- I have no idea if any of this applies to mac, it's mainly for linux
-		linkoptions "-l:garrysmod/bin/lua_shared.so"
 		postbuildcommands "{MOVE} %{cfg.targetdir}/lib%{prj.name}%{cfg.targetsuffix}.so %{cfg.targetdir}/%{prj.name}%{cfg.targetsuffix}.dll"
 	else
 		links "lua_shared"
@@ -29,9 +22,45 @@ project "gmsv_gcrash"
 	}
 
 	configuration "Debug"
+		architecture "x86"
 		optimize "Debug"
+		if _ACTION == "gmake2" then
+			linkoptions "-l:garrysmod/bin/lua_shared.so"
+		end
+		if os.istarget( "windows" ) then targetsuffix "_win32" end
+		if os.istarget( "macosx" )  then targetsuffix "_osx"   end
+		if os.istarget( "linux" )   then targetsuffix "_linux" end
+
+	configuration "Debug64"
+		architecture "x86_64"
+		optimize "Debug"
+		if _ACTION == "gmake2" then
+			linkoptions "-l:bin/linux64/lua_shared.so"
+		end
+		if os.istarget( "windows" ) then targetsuffix "_win64" end
+		if os.istarget( "macosx" )  then targetsuffix "_osx64"   end
+		if os.istarget( "linux" )   then targetsuffix "_linux64" end
 
 	configuration "Release"
+		architecture "x86"
 		optimize "Speed"
 		if staticruntime then staticruntime "On"
 		else flags "StaticRuntime" end
+		if _ACTION == "gmake2" then
+			linkoptions "-l:garrysmod/bin/lua_shared.so"
+		end
+		if os.istarget( "windows" ) then targetsuffix "_win32" end
+		if os.istarget( "macosx" )  then targetsuffix "_osx"   end
+		if os.istarget( "linux" )   then targetsuffix "_linux" end
+	
+	configuration "Release64"
+		architecture "x86_64"
+		optimize "Speed"
+		if staticruntime then staticruntime "On"
+		else flags "StaticRuntime" end
+		if _ACTION == "gmake2" then
+			linkoptions "-l:bin/linux64/lua_shared.so"
+		end
+		if os.istarget( "windows" ) then targetsuffix "_win64" end
+		if os.istarget( "macosx" )  then targetsuffix "_osx64"   end
+		if os.istarget( "linux" )   then targetsuffix "_linux64" end
